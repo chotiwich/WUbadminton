22f7cb7b2c1bb2e97353cc28a1af937786f80b98
unknown <chotiwich6793@gmail.com>
2020-10-06 20:50:50 +0700

    แก้ไขหน้า ยืม-คืน

--- Screen/drawerScreens/Borrow.js
+++ Screen/drawerScreens/Borrow.js
@@ -23,61 +23,92 @@ export default class Borrow extends Component {
         source={require('D:/application/WUbadminton/Image/back07.png')}
         style={{flex: 1, resizeMode: 'cover'}}>
         <View>
-          <Text
+          <View
             style={{
-              fontSize: 20,
-              marginTop: 20,
-              paddingBottom: 10,
-              borderBottomColor: '#5e3881',
-              borderBottomWidth: 1,
+              backgroundColor: 'white',
+              textAlign: 'center',
               minHeight: 10,
-              minWidth: 50,
-              elevation: 50,
-              borderRadius: 50,
-            }}>
-            {' '}
-            อุปกรณ์ที่ต้องการยืม :{' '}
-          </Text>
-          <Picker
-            selectedValue={this.state.selectedLabel}
-            onValueChange={this.Show.bind()}>
-            <Picker.Item label="อุปกรณ์ที่ต้องการยืม" value="0"></Picker.Item>
-            <Picker.Item
-              label="ไม้แบดมินตัน"
-              value="ไม้แบดมินตัน"></Picker.Item>
-            <Picker.Item label="ลูกเปตอง" value="ลูกเปตอง"></Picker.Item>
-            <Picker.Item label="ลูกแกนเปตอง" value="ลูกแกนเปตอง"></Picker.Item>
-          </Picker>
-          <Text
-            style={{
-              fontSize: 20,
-              marginTop: 20,
-              paddingBottom: 10,
-              borderBottomColor: '#5e3881',
-              borderBottomWidth: 1,
-              minHeight: 10,
-              minWidth: 50,
+              minWidth: 10,
               elevation: 50,
               borderRadius: 20,
+              fontSize: 20,
+              marginTop: 15,
+              marginLeft: 15,
+              marginRight: 15,
+              color: '#5e3881',
             }}>
-            {' '}
-            จำนวน (ชุด) :{' '}
-          </Text>
-          <Picker
-            selectedValue={this.state.selectedLabel}
-            onValueChange={this.Show.bind()}>
-            <Picker.Item label="1" value="1 ชุด"></Picker.Item>
-            <Picker.Item label="2" value="2 ชุด"></Picker.Item>
-            <Picker.Item label="3" value="3 ชุด"></Picker.Item>
-            <Picker.Item label="4" value="4 ชุด"></Picker.Item>
-            <Picker.Item label="5" value="5 ชุด"></Picker.Item>
-            <Picker.Item label="6" value="6 ชุด"></Picker.Item>
-          </Picker>
-          <Button
-            title="ตกลง"
-            color="#5e3881"
-            onPress={() => navigation.navigate('')}
-          />
+            <Text
+              style={{
+                fontSize: 20,
+                marginTop: 20,
+                paddingBottom: 10,
+                color: '#5e3881',
+              }}>
+              {' '}
+              อุปกรณ์ที่ต้องการยืม :{' '}
+            </Text>
+            <Picker
+              selectedValue={this.state.selectedLabel}
+              onValueChange={this.Show.bind()}>
+              <Picker.Item
+                label="กรุณาเลือกอุปกรณ์ที่ต้องการยืม"
+                value="กรุณาเลือกอุปกรณ์ที่ต้องการยืม"></Picker.Item>
+              <Picker.Item
+                label="ไม้แบดมินตัน"
+                value="ไม้แบดมินตัน"></Picker.Item>
+              <Picker.Item label="ลูกเปตอง" value="ลูกเปตอง"></Picker.Item>
+              <Picker.Item
+                label="ลูกแกนเปตอง"
+                value="ลูกแกนเปตอง"></Picker.Item>
+            </Picker>
+          </View>
+
+          <View
+            style={{
+              backgroundColor: 'white',
+              textAlign: 'center',
+              minHeight: 10,
+              minWidth: 10,
+              elevation: 50,
+              borderRadius: 20,
+              fontSize: 20,
+              marginTop: 15,
+              marginLeft: 15,
+              marginRight: 15,
+              color: '#5e3881',
+            }}>
+            <Text
+              style={{
+                fontSize: 20,
+                marginTop: 20,
+                paddingBottom: 10,
+                color: '#5e3881',
+              }}>
+              {' '}
+              จำนวนที่ยืม (ชุด) :{' '}
+            </Text>
+            <Picker
+              selectedValue={this.state.selectedLabel}
+              onValueChange={this.Show.bind()}>
+              <Picker.Item label="1" value="1 ชุด"></Picker.Item>
+              <Picker.Item label="2" value="2 ชุด"></Picker.Item>
+              <Picker.Item label="3" value="3 ชุด"></Picker.Item>
+              <Picker.Item label="4" value="4 ชุด"></Picker.Item>
+              <Picker.Item label="5" value="5 ชุด"></Picker.Item>
+              <Picker.Item label="6" value="6 ชุด"></Picker.Item>
+            </Picker>
+          </View>
+
+          <View
+            style={[
+              {width: '95%', borderRadius: 30, margin: 10, marginTop: 20},
+            ]}>
+            <Button
+              title="ตกลง"
+              color="#5e3881"
+              onPress={() => navigation.navigate('')}
+            />
+          </View>
         </View>
       </ImageBackground>
     );
--- Screen/drawerScreens/Rebad.js
+++ Screen/drawerScreens/Rebad.js
@@ -3,19 +3,114 @@
 
 //Import React
 import React from 'react';
+import {Component} from 'react';
 
 //Import all required component
-import { View, Text } from 'react-native';
+import {View, Text, Picker, ImageBackground, Button} from 'react-native';
 
-const Rebad = () => {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', marginTop: 100 }}>
-      <Text style={{ fontSize: 23, marginTop: 10 }}> คืนอุปกรณ์ </Text>
-      <Text style={{ fontSize: 18, marginTop: 10 }}>
-        Simple Login Registraction Example
-      </Text>
-      <Text style={{ fontSize: 18, marginTop: 10 }}>https://aboutreact</Text>
-    </View>
-  );
-};
-export default Rebad ;
+export default class Rebad extends Component {
+  constructor() {
+    super();
+    this.state = {selectedLabel: ''};
+  }
+  Show = (value) => {
+    alert(value);
+    this.setState({selectedLabel: value});
+  };
+  render() {
+    return (
+      <ImageBackground
+        source={require('D:/application/WUbadminton/Image/back07.png')}
+        style={{flex: 1, resizeMode: 'cover'}}>
+        <View>
+          <View
+            style={{
+              backgroundColor: 'white',
+              textAlign: 'center',
+              minHeight: 10,
+              minWidth: 10,
+              elevation: 50,
+              borderRadius: 20,
+              fontSize: 20,
+              marginTop: 15,
+              marginLeft: 15,
+              marginRight: 15,
+              color: '#5e3881',
+            }}>
+            <Text
+              style={{
+                fontSize: 20,
+                marginTop: 20,
+                paddingBottom: 10,
+                color: '#5e3881',
+              }}>
+              {' '}
+              อุปกรณ์ที่ต้องการคืน :{' '}
+            </Text>
+            <Picker
+              selectedValue={this.state.selectedLabel}
+              onValueChange={this.Show.bind()}>
+              <Picker.Item
+                label="กรุณาเลือกอุปกรณ์ที่ต้องการคืน"
+                value="กรุณาเลือกอุปกรณ์ที่ต้องการคืน"></Picker.Item>
+              <Picker.Item
+                label="ไม้แบดมินตัน"
+                value="ไม้แบดมินตัน"></Picker.Item>
+              <Picker.Item label="ลูกเปตอง" value="ลูกเปตอง"></Picker.Item>
+              <Picker.Item
+                label="ลูกแกนเปตอง"
+                value="ลูกแกนเปตอง"></Picker.Item>
+            </Picker>
+          </View>
+
+          <View
+            style={{
+              backgroundColor: 'white',
+              textAlign: 'center',
+              minHeight: 10,
+              minWidth: 10,
+              elevation: 50,
+              borderRadius: 20,
+              fontSize: 20,
+              marginTop: 15,
+              marginLeft: 15,
+              marginRight: 15,
+              color: '#5e3881',
+            }}>
+            <Text
+              style={{
+                fontSize: 20,
+                marginTop: 20,
+                paddingBottom: 10,
+                color: '#5e3881',
+              }}>
+              {' '}
+              จำนวนที่ต้องการคืน (ชุด) :{' '}
+            </Text>
+            <Picker
+              selectedValue={this.state.selectedLabel}
+              onValueChange={this.Show.bind()}>
+              <Picker.Item label="1" value="1 ชุด"></Picker.Item>
+              <Picker.Item label="2" value="2 ชุด"></Picker.Item>
+              <Picker.Item label="3" value="3 ชุด"></Picker.Item>
+              <Picker.Item label="4" value="4 ชุด"></Picker.Item>
+              <Picker.Item label="5" value="5 ชุด"></Picker.Item>
+              <Picker.Item label="6" value="6 ชุด"></Picker.Item>
+            </Picker>
+          </View>
+
+          <View
+            style={[
+              {width: '95%', borderRadius: 30, margin: 10, marginTop: 20},
+            ]}>
+            <Button
+              title="ตกลง"
+              color="#5e3881"
+              onPress={() => navigation.navigate('')}
+            />
+          </View>
+        </View>
+      </ImageBackground>
+    );
+  }
+}
