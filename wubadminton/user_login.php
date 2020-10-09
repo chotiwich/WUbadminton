6c4c7838bf6053e535db70fb82da641e452b4df3
unknown <chotiwich6793@gmail.com>
2020-10-09 18:24:14 +0700

    updates API

--- wubadminton/user_login.php
+++ wubadminton/user_login.php
@@ -1,54 +1,47 @@
 <?php
- 
-// Importing DBConfig.php file.
 include 'DBConfig.php';
- 
-// Creating connection.
- $con = mysqli_connect($HostName, $DatabaseName, $HostUser, $HostPass);
- 
- // Getting the received JSON into $json variable.
- $json = file_get_contents('php://input');
- 
- // decoding the received JSON and store into $obj variable.
- $obj = json_decode($json,true);
- 
-// Populate User email from JSON $obj array and store into $email.
-$email = $obj['email'];
- 
-// Populate Password from JSON $obj array and store into $password.
-$password = $obj['password'];
+$message ='';
 
-//Applying User Login query with email and password match.
-$Sql_Query = "select * from user_login where email = '$email' and password = '$password' ";
+$conn =new mysqli($host_name,$host_user,$host_password,$database_name);
 
-// Executing SQL Query.
-$check = mysqli_fetch_array(mysqli_query($con,$Sql_Query));
 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$json = file_get_contents('php://input');
+
+$obj = json_decode($json,true);
+
+	 
+	 
+	
+$Sql_Query = "select * from user_login where email = '$_GET[user_email]' and password = '$_GET[user_password]' ";
+$check = mysqli_fetch_array(mysqli_query($conn,$Sql_Query));
+// Executing SQL Query.use
 
 if(isset($check)){
 
- $SuccessLoginMsg = 'Data Matched';
+ 	$SuccessLoginMsg = 'Data Matched';
  
  // Converting the message into JSON format.
-$SuccessLoginJson = json_encode($SuccessLoginMsg);
+	$SuccessLoginJson = json_encode($SuccessLoginMsg);
  
 // Echo the message.
- echo $SuccessLoginJson ; 
+ 	echo $SuccessLoginJson ; 
 
- }
- 
- else{
+ }else{
  
  // If the record inserted successfully then show the message.
-$InvalidMSG = 'Invalid Username or Password Please Try Again' ;
+	$InvalidMSG = 'Invalid Username or Password Please Try Again' ;
  
 // Converting the message into JSON format.
-$InvalidMSGJSon = json_encode($InvalidMSG);
+	$InvalidMSGJSon = json_encode($InvalidMSG);
  
 // Echo the message.
- echo $InvalidMSGJSon ;
+ 	echo $InvalidMSGJSon ;
  
  }
- 
- mysqli_close($con);
+mysqli_close($conn);
+	
 ?>
--- wubadminton/wufoodapi/DB_config.php
+++ wubadminton/wufoodapi/DB_config.php
@@ -1,15 +1,18 @@
+
+
+
 <?php
  
 //Define your host here.
-$host_name = "localhost";
+$HostName = "localhost";
  
 //Define your database name here.
-$database_name = "wubadminton";
+$DatabaseName = "wufood";
  
 //Define your database username here.
-$host_user = "root";
+$HostUser = "root";
  
 //Define your database password here.
-$host_password = "";
+$HostPass = "";
  
 ?>
